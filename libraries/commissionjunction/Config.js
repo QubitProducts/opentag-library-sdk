916ebed9daec0091532e7133a5e605380fe8f0ab
mmara87 <mmara@cj.com>
2018-04-23 12:14:36 -0700

    Update Config.js

--- libraries/commissionjunction/Config.js
+++ libraries/commissionjunction/Config.js
@@ -2,7 +2,7 @@ var Config = {
 /*config*/
 "id": 6,
 "name": "CJ Affiliate",
-"imageUrl": "https://s3-eu-west-1.amazonaws.com/opentag-images/CommissionJunction.png",
+"imageUrl": "http://shell1.vclk.net/~mmara/images/cj-affiliate-logo-commission-junction.png",
 "description": "CJ Affiliate by Conversant (formerly Commission Junction) is the leading global affiliate marketing network, specializing in pay-for-performance programs that drive results for businesses around the world."
 /*~config*/
 };
